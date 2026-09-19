8006d9f43e419f518342aee814ee7e1a9354a349
Saud Zaid <Saud.zaid.442@gmail.com>
2026-09-19 21:04:31 +0300

    Format output with each statement on a separate line

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -4,7 +4,7 @@ version = 3
 pi = 3.14159
 computation_valid = 2 + 2 == 4
 
-print(
-    f"Language: {language} Version: {version} "
-    f"Pi approx: {pi:.2f} Computation valid: {computation_valid}"
-)
+print(f"Language: {language}")
+print(f"Version: {version}")
+print(f"Pi approx: {pi:.2f}")
+print(f"Computation valid: {computation_valid}")
