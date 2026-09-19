dc5873f92d0d707855f10ed4ba70666a1b65c0db
Saud Zaid <Saud.zaid.442@gmail.com>
2026-09-19 21:29:44 +0300

    Remove trailing newline from print_alphabt.py

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 for i in range(97, 123):
     if chr(i) != 'e' and chr(i) != 'q':
-        print("{}".format(chr(i)), end="" if i != 122 else "\n")
+        print("{}".format(chr(i)), end="")
